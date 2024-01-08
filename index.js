3c01aae90ad29dc2b7c3b423bff237982868a12b
Brennan <brennanosunkwo14@gmail.com>
2024-01-08 14:54:09 -0500

    version 2 completed exercisse tracker

--- index.js
+++ index.js
@@ -13,110 +13,126 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
 
 const exerciseSchema = new mongoose.Schema({
-  description: {
+  user_id: {
     type: String,
     required: true
   },
-  duration: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: String
-  }
+  description: String,
+  duration: Number,
+  date: Date
 })
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  log: [exerciseSchema]
+  username: String
 })
 
 const Session = mongoose.model('Session', exerciseSchema );
 const User = mongoose.model('User', userSchema);
 
-app.post('/api/users/', bodyParser.urlencoded({extended: false}), (req, res) =>{
-  
-  let newUser = new User({username: req.body.username})
-  newUser.save((err, saved) =>{
-    if(!err){
-      res.json({
-        username: saved.username,
-        id: saved.id
-      })
-    }
-  });
-})
-
-app.get('/api/users', (req, res) =>{
-  User.find({}, (err, array) =>{
-    if(!err){
-      res.json(array)
-    }
+app.post('/api/users/', bodyParser.urlencoded({extended: false}), async (req, res) =>{
+  const userObject = new User({
+    username: req.body.username
   })
-})
-app.post('/api/users/:_id/exercises', bodyParser.urlencoded({extended: false}), (req, res) =>{
-  let newSession = new Session({
-    description: req.body.description,
-    duration: parseInt(req.body.duration),
-    date: req.body.date,
-  })
-  if(newSession.date === ""){
-    newSession.date = new Date().toISOString().substring(0,10)
+  try{
+    const user = await userObject.save()
+    res.json(user);
+  }catch(error){
+    console.log(error)
   }
-  User.findByIdAndUpdate(
-    req.body[':_id'],
-    {$push: {log: newSession}},
-    {new: true},
-    (err, upUser) =>{
-      let resObject = {}
-      resObject['_id'] = upUser.id;
-      resObject['username'] = upUser.username;
-      resObject['date'] = new Date(newSession.date).toDateString();
-      resObject['description'] = newSession.description;
-      resObject['duration'] = newSession.duration;
-      res.json(resObject);
-    }
-  )
+  
 })
 
-app.get("/api/users/:_id/logs", (req, res) =>{
-  User.findById(req.params['_id'], (err, result) =>{
-    let resObject = result;
-    if(req.query.from || req.query.to){
-      let fromDate  = new Date(0);
-      let toDate = new Date();
-      if(req.query.from){
-        fromDate = new Date(req.query.from)
-      }
-      if(req.query.to){
-        toDate = new Date(req.query.to)
-      }
+app.get('/api/users', async (req, res) =>{
+  
+  const users = await User.find({}).select("_id username");
+  if(!users){
+    res.send("No users");
+    console.log("No users")
+  }
+  else{
+    res.json(users);
+  }
+})
+app.post('/api/users/:_id/exercises', bodyParser.urlencoded({extended: false}), async (req, res) =>{
+  const id = req.params._id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  const date  = req.body.date;
 
-      fromDate = fromDate.getTime();
-      toDate = toDate.getTime();
-
-      resObject.log = resObject.log.filter((sessions) =>{
-        let sesDate = new Date(sessions.date).getTime();
-
-        return sesDate >= fromDate && sesDate <= toDate;
+  try{
+    const user = await User.findById(id);
+    if(!user){
+      res.send('Could not find user');
+      console.log('Could not find user')
+    }
+    else{
+      const sessionObject = new Session({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date() : new Date()
+      })
+      const session = await sessionObject.save()
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: session.description,
+        duration: session.duration,
+        date: new Date(session.date).toDateString()
       })
     }
-    if(req.query.limit){
-      resObject.log = resObject.log.slice(0, req.query.limit)
-    }
-    resObject['count'] = result.log.length
-    res.json(resObject)
+  } catch(error){
+    console.error(error)
+  }
+})
+
+app.get("/api/users/:_id/logs", async(req, res) =>{
+  
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit
+  const id = req.params._id;
+
+  const user = await User.findById(id);
+
+  if(!user){
+    res.send("couldnt find user")
+    console.log("couldnt find user");
+    return;
+  }
+  let resObject = {};
+
+  if(from){
+    resObject['$gte'] = new Date(from);
+  }
+  if(to){
+    resObject['$lte'] = new Date(to);
+  }
+
+  let filteringObject = {
+    user_id: id,
+  }
+  if(from || to){
+    filteringObject.date = resObject
+  }
+
+  const sessions = await Session.find(filteringObject).limit(+limit ?? 500);
+
+  const log = sessions.map(d => ({
+    description: d.description,
+    duration: d.duration,
+    date: d.date.toDateString()
+  }))
+
+  res.json({
+    username: user.username,
+    count: sessions.length,
+    _id: user._id,
+    log
   })
 })
